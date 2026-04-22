741c9620df60683cbe4bcea2312c40d65a638c20
Lukáš Benda <126458059+MadLukee@users.noreply.github.com>
2026-04-22 11:47:38 +0200

    abstraktni trida zamestnance

---
+++ src/main/java/cz/upce/pc2t/Zamestnanec.java
@@ -0,0 +1,27 @@
+package cz.upce.pc2t;
+
+public abstract class Zamestnanec {
+
+    private static int dalsiId = 1;
+
+    private final int id;
+    private String jmeno;
+    private String prijmeni;
+    private int rokNarozeni;
+
+    protected Zamestnanec(String jmeno, String prijmeni, int rokNarozeni) {
+        this.id = dalsiId++;
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.rokNarozeni = rokNarozeni;
+    }
+
+    public abstract String getSkupina();
+
+    public abstract String popisDovednosti();
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s (nar. %d) - %s", id, jmeno, prijmeni, rokNarozeni, getSkupina());
+    }
+}
