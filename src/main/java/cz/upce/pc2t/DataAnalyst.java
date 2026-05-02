bdd038226782d331c3ca7137c7c725227fdc86ff
Lukáš Benda <126458059+MadLukee@users.noreply.github.com>
2026-05-03 00:01:09 +0200

    spolecni spolupracovnici datoveho analytika

--- src/main/java/cz/upce/pc2t/DataAnalyst.java
+++ src/main/java/cz/upce/pc2t/DataAnalyst.java
@@ -1,5 +1,10 @@
 package cz.upce.pc2t;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class DataAnalyst extends Zamestnanec {
 
     public DataAnalyst(String jmeno, String prijmeni, int rokNarozeni) {
@@ -14,5 +19,22 @@ public class DataAnalyst extends Zamestnanec {
     @Override
     public String popisDovednosti() {
         return "Analýza dat, hledání společných spolupracovníků";
+    }
+
+     public List<Zamestnanec> najdiSpolecneSpolupracovniky(Zamestnanec druhy) {
+        Set<Integer> mojeIds = new HashSet<>();
+        for (Spoluprace s : this.getSpoluprace()) {
+            mojeIds.add(s.getSpolupracovnik().getId());
+        }
+
+        List<Zamestnanec> spolecni = new ArrayList<>();
+        Set<Integer> pridani = new HashSet<>();
+        for (Spoluprace s : druhy.getSpoluprace()) {
+            int id = s.getSpolupracovnik().getId();
+            if (mojeIds.contains(id) && pridani.add(id)) {
+                spolecni.add(s.getSpolupracovnik());
+            }
+        }
+        return spolecni;
     }
 }
